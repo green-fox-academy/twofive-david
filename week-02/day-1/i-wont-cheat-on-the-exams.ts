c415eaa9e8a3cd89d1055282c345ec2a56d03c7a
David Pal <paldavid2030@gmail.com>
2018-10-29 13:46:00 +0100

    add i-wont-cheat-on-the-exams

---
+++ week-02/day-1/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let x = 1; x <= 100; x++) {
+    console.log("I won't cheat on the exam!");
+}
