8eb3727a72c7c4cc93f4d0a88871f210f018ee3f
David Pal <paldavid2030@gmail.com>
2018-10-30 14:16:16 +0100

    add increment-element

---
+++ week-02/day-2/increment-element.ts
@@ -0,0 +1,17 @@
+export{};
+'use strict'
+
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the console
+
+//  - Increments any index passed on as an argument to the function
+
+let numList: number[] = [1, 2, 3, 4, 5];
+
+function incrementByOne(n: number) {
+numList[n] += 1;
+return numList;
+}
+
+console.log(incrementByOne(3));
