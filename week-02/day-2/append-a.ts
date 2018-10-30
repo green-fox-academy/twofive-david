6c284474a88539f9808a0364983e247a33e6bbbe
David Pal <paldavid2030@gmail.com>
2018-10-30 17:51:35 +0100

    append-a.ts

--- week-02/day-2/append-a.ts
+++ week-02/day-2/append-a.ts
@@ -4,7 +4,7 @@ export{};
 
 // -  Create an array variable named `animals`
 //    with the following content: `["koal", "pand", "zebr"]`
-// -  Add all elements an `"a"` at the end
+// -  Add all elements an `"a"` at the end.
 
 let animals: string[] = ["koal", "pand", "zebr"];
 
