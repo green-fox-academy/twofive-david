d2fe887146ce02b8a3de2cbddde3c7cc7f704c6f
David Pal <paldavid2030@gmail.com>
2018-10-30 08:53:32 +0100

    update draw-diamond

--- week-02/day-1/draw-diamond.ts
+++ week-02/day-1/draw-diamond.ts
@@ -2,14 +2,16 @@ export {};
 
 'use strict';
 
-let lineCount: number = 7;
+let lineCount: number = 8;
 let x: number = lineCount;
 let n: number = 1;
 
 let space: string = " ";
 let star: string = "*";
 
-for(let i = 1; i <= lineCount; i++) {
+if(lineCount % 2 === 0){
+
+for(let i = 1; i <= lineCount + 1; i++) {
 
     if (i <= lineCount / 2) {
         console.log(space.repeat(x) + star.repeat(n));
@@ -22,6 +24,24 @@ for(let i = 1; i <= lineCount; i++) {
              x ++;
              n -= 2;
              //console.log(n);
+    }
+}
+}
+else {
+    for(let i = 1; i <= lineCount; i++) {
+
+        if (i <= lineCount / 2) {
+            console.log(space.repeat(x) + star.repeat(n));
+            x --;
+            n += 2;
+        }
+    
+        else {
+            console.log(space.repeat(x) + star.repeat(n));
+                 x ++;
+                 n -= 2;
+                 //console.log(n);
+        }
     }
 }
 
