70e87626dd01d882a34570d843377677b74c4c1f
David Pal <paldavid2030@gmail.com>
2018-10-30 17:52:55 +0100

    add double-items

--- week-02/day-2/double-items.ts
+++ week-02/day-2/double-items.ts
@@ -4,7 +4,7 @@ export{};
 
 // -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
 // -  Double all the strings in the array, use a built in array method instead of a loop
-// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`.
 
 let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
 
