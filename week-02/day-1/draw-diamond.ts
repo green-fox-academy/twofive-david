232c213adad16830270ce9ffef9d69a8066ffb9d
David Pal <paldavid2030@gmail.com>
2018-10-30 09:03:50 +0100

    update draw-diamond for even numbers

--- week-02/day-1/draw-diamond.ts
+++ week-02/day-1/draw-diamond.ts
@@ -2,7 +2,7 @@ export {};
 
 'use strict';
 
-let lineCount: number = 8;
+let lineCount: number = 7;
 let x: number = lineCount;
 let n: number = 1;
 
@@ -11,9 +11,13 @@ let star: string = "*";
 
 if(lineCount % 2 === 0){
 
-for(let i = 1; i <= lineCount + 1; i++) {
+for(let i = 1; i <= lineCount; i++) {
 
-    if (i <= lineCount / 2) {
+    if (i == lineCount / 2) {
+        console.log(space.repeat(x) + star.repeat(n));
+    }
+
+    else if (i < lineCount / 2) {
         console.log(space.repeat(x) + star.repeat(n));
         x --;
         n += 2;
