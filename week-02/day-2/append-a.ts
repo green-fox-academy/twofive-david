376e1961c75dcbd90ce54aa544b6719420f9cf7c
David Pal <paldavid2030@gmail.com>
2018-10-30 14:36:26 +0100

    add doubÃle-items

---
+++ week-02/day-2/append-a.ts
@@ -0,0 +1,16 @@
+export{};
+
+'use strict'
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+let animals_a = animals.map(function(element){
+    element += "a";
+    return element;
+})
+
+console.log(animals_a);
---
+++ week-02/day-2/double-items.ts
@@ -0,0 +1,16 @@
+export{};
+
+'use strict'
+
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+drinks.forEach(function(element, index, array){
+   array[index] += element;
+    //return element;
+});
+
+console.log(drinks);
