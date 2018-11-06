7b08cdda4b97f0d4e6b1774e2ceb2b7ea1af35ff
David Pal <paldavid2030@gmail.com>
2018-11-06 14:51:54 +0100

    add element-finder

---
+++ week-03/day-2/element-finder.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Write a function that checks if the array contains "7"
+// if it contains return "Hoorray" otherwise return "Noooooo"
+
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 8];
+
+function containsSeven (arr: number[]): string {
+  if (arr.indexOf(7) === -1) return "Noooooo";
+  else return "Hoorray";
+}
+
+console.log(containsSeven(numbers));
+// The output should be: "Noooooo"
+// Do this again with a different solution using different list functions!
