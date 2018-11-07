f4c213bdac673bff0b771fbc143cd7e3463d514a
David Pal <paldavid2030@gmail.com>
2018-11-07 22:05:59 +0100

    add starry-night

---
+++ week-03/day-3/starry-night/starry-night-ts.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the night sky:
+//  - The background should be black
+//  - The stars should be small squares
+//  - The stars should have random positions on the canvas
+//  - The stars should have random color (some shade of grey)
+
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+for (let i: number = 0; i <= 1000; i++) {
+  let starSize: number = getRandomArbitrary(0, 2);
+  let starPositionX: number = getRandomArbitrary(0, 600);
+  let starPositionY: number = getRandomArbitrary(0, 400);
+  let randomGrey: number = getRandomArbitrary(0, 255);
+
+  ctx.fillStyle = `rgb(${randomGrey}, ${randomGrey}, ${randomGrey})`
+  ctx.fillRect(starPositionX, starPositionY, starSize, starSize);
+}
