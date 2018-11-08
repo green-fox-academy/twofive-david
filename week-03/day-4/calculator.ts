a8649c98c4adfb4549566720bdc0e474893d6614
David Pal <paldavid2030@gmail.com>
2018-11-08 12:20:38 +0100

    add calculator

---
+++ week-03/day-4/calculator.ts
@@ -0,0 +1,58 @@
+'use strict';
+export { };
+// Create a simple calculator application which does read the parameters from the standard input
+// and prints the result to the console.
+
+// It should support the following operations, create function called "calculate()" :
+// +, -, *, /, % and it should support two operands:
+
+// The format of the expressions must be: {operation} {operand} {operand}.
+// Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+// You should use the command line arguments to accept user input
+
+// It should work like this:
+
+// Start the program with "node calculator.js + 5 6"
+// If number of arguments are not correct, print some nice errors
+// Else print the result
+// Say goodbye
+
+const args = process.argv.splice(2); // Just a helper for you to get started
+
+function calculator(args: string[]): any {
+
+  if (args.length < 3) {
+    return 'Give me more arguments, biaaaatcchhhh!!!';
+  }
+
+  else {
+
+    switch (args[0]) {
+      case '+':
+        console.log(+ args[1] + + args[2]);
+        return 'Thanks for using the calculator. Goodbye.';
+
+      case '-':
+        console.log(+ args[1] - + args[2]);
+        return 'Thanks for using the calculator. Goodbye.';
+
+      case 'x':
+        console.log(+ args[1] * + args[2]);
+        return 'Thanks for using the calculator. Goodbye.';
+
+      case 'slash':
+        console.log(+ args[1] / + args[2]);
+        return 'Thanks for using the calculator. Goodbye.';
+
+      case '%':
+        console.log(+ args[1] % + args[2]);
+        return 'Thanks for using the calculator.Goodbye.';
+    }
+
+  }
+}
+
+//console.log('Input params are', args);
+console.log(calculator(args));
+
