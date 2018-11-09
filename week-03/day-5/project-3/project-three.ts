6274a1b485d1a23b4ad54a208ca08a3dd9eb8093
David Pal <paldavid2030@gmail.com>
2018-11-09 17:20:16 +0100

    add work in progress triangle

---
+++ week-03/day-5/project-3/project-three.ts
@@ -0,0 +1,37 @@
+'use strict'
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+let side: number  = 30;
+//draw **One** triangle with two parameters: starting point x and Y coordinates - this is its left side corner)
+
+
+//oneTriangle(canvas.width/2, canvas.height/2);
+
+function bigTriangle(fromX: number, fromY, size: number): any {
+
+  function oneTriangle(): void {
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(fromX + side, fromY);
+    //ctx.moveTo(fromX + 30, fromY);
+    ctx.lineTo(fromX + side/2, fromY - Math.sqrt(675));
+    //ctx.moveTo(fromX + 15, fromY - Math.sqrt(675));
+    ctx.lineTo(fromX, fromY);
+    ctx.stroke();
+  }
+
+    for (let i: number = 1; i <= size; i ++) {
+      
+      fromX -= 45;
+      fromY += Math.sqrt(675);
+      
+      for (let j: number = 0; j < i; j ++) {
+    
+      oneTriangle();
+      fromX += side;
+      }
+    }
+}
+
+bigTriangle(canvas.width/2, canvas.height/2, 5);
