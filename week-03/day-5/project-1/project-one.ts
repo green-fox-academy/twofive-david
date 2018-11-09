01f97f8a24a7d9b50337ff466445a78b054d1b7c
David Pal <paldavid2030@gmail.com>
2018-11-09 10:53:49 +0100

    add project-1: line play

--- week-03/day-5/project-1/project-one.ts
+++ week-03/day-5/project-1/project-one.ts
@@ -3,5 +3,28 @@
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 50, 50);
+function makeLinePurple(xFrom: number, yFrom: number, xTo: number, yTo: number): void {
+  ctx.beginPath();
+  ctx.strokeStyle = "purple";
+  ctx.moveTo(xFrom, yFrom);
+  ctx.lineTo(xTo, yTo);
+  ctx.stroke();
+}
+
+function makeLineGreen(xFrom: number, yFrom: number, xTo: number, yTo: number): void {
+  ctx.beginPath();
+  ctx.strokeStyle = "green";
+  ctx.moveTo(xFrom, yFrom);
+  ctx.lineTo(xTo, yTo);
+  ctx.stroke();
+}
+
+for (let i: number = 20; i < 400; i += 20) {
+  makeLinePurple(i, 0, 400, i);
+}
+
+for (let j: number = 20; j < 400; j += 20) {
+  makeLineGreen(0, j, j, 400);
+}
+
+// figure out how to save line density in a variable
