211d39fe75f18148a3af39b8cf7784af806d7bed
David Pal <paldavid2030@gmail.com>
2018-11-10 16:21:22 +0100

    add project-three: draw a pyramid of triangles

--- week-03/day-5/project-3/project-three.ts
+++ week-03/day-5/project-3/project-three.ts
@@ -2,36 +2,39 @@
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-let side: number  = 30;
-//draw **One** triangle with two parameters: starting point x and Y coordinates - this is its left side corner)
+let side: number = 30;
 
+//function draws one triangle with one parameter: the size  (how many trinagles to stack on each other)
 
-//oneTriangle(canvas.width/2, canvas.height/2);
+function bigTriangle(size: number): any {
 
-function bigTriangle(fromX: number, fromY, size: number): any {
+  let fromX: number = canvas.width / 2 - side / 2;
+  let fromY: number = 0;
 
   function oneTriangle(): void {
+
     ctx.beginPath();
     ctx.moveTo(fromX, fromY);
     ctx.lineTo(fromX + side, fromY);
     //ctx.moveTo(fromX + 30, fromY);
-    ctx.lineTo(fromX + side/2, fromY - Math.sqrt(675));
+    ctx.lineTo(fromX + side / 2, fromY - Math.sqrt(675));
     //ctx.moveTo(fromX + 15, fromY - Math.sqrt(675));
     ctx.lineTo(fromX, fromY);
     ctx.stroke();
   }
 
-    for (let i: number = 1; i <= size; i ++) {
-      
-      fromX -= 45;
-      fromY += Math.sqrt(675);
-      
-      for (let j: number = 0; j < i; j ++) {
-    
+  for (let i: number = 1; i <= size; i++) {
+
+    fromX -= side * i - 15;
+    fromY += Math.sqrt(675);
+
+    for (let j: number = 0; j < i; j++) {
+
       oneTriangle();
       fromX += side;
-      }
     }
+
+  }
 }
 
-bigTriangle(canvas.width/2, canvas.height/2, 5);
+bigTriangle(20);
