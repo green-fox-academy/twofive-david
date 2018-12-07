f74e4f38f76d183f83ae82fcc3749709e1d1a200
David Pal <paldavid2030@gmail.com>
2018-12-07 14:01:03 +0100

    feat(img-slider): add main.js

--- week-07/day-3/main.js
+++ week-07/day-3/main.js
@@ -1,1 +1,38 @@
-// to be filled in
+'use strict'
+
+const thumbnails = document.querySelector('#thumbnails');
+
+const myIMages = [
+  {
+    title: 'African Ballsack Storck',
+    src: 'img/african-ballsack-storck.jpg',
+    description: "Lorizzle ma nizzle dolor crackalackin yo, we gonna chung adipiscing elit. Rizzle sapien velizzle, dope volutpizzle, suscipizzle bling bling, tellivizzle vizzle, funky fresh. Pellentesque eget tortizzle. Sizzle erizzle."
+  },
+  {
+    title: 'Amboseli Flamingos',
+    src: 'img/amboseli-flamingos.jpg',
+    description: "Lorizzle ma nizzle dolor crackalackin yo, we gonna chung adipiscing elit. Rizzle sapien velizzle, dope volutpizzle, suscipizzle bling bling, tellivizzle vizzle, funky fresh. Pellentesque eget tortizzle. Sizzle erizzle."
+  },
+  {
+    title: 'Amboseli Flamingos 2',
+    src: 'img/amboseli-flamingos2.jpg',
+    description: "Lorizzle ma nizzle dolor crackalackin yo, we gonna chung adipiscing elit. Rizzle sapien velizzle, dope volutpizzle, suscipizzle bling bling, tellivizzle vizzle, funky fresh. Pellentesque eget tortizzle. Sizzle erizzle."
+  },
+  {
+    title: 'Giraffe Couple',
+    src: 'img/giraffe-couple.jpg',
+    description: "Lorizzle ma nizzle dolor crackalackin yo, we gonna chung adipiscing elit. Rizzle sapien velizzle, dope volutpizzle, suscipizzle bling bling, tellivizzle vizzle, funky fresh. Pellentesque eget tortizzle. Sizzle erizzle."
+  },
+  {
+    title: 'Male Ostrich',
+    src: 'img/male-ostrich.jpg',
+    description: "Lorizzle ma nizzle dolor crackalackin yo, we gonna chung adipiscing elit. Rizzle sapien velizzle, dope volutpizzle, suscipizzle bling bling, tellivizzle vizzle, funky fresh. Pellentesque eget tortizzle. Sizzle erizzle."
+  },
+  {
+    title: 'Savannah with Zebras',
+    src: 'img/savannah.jpg',
+    description: "Lorizzle ma nizzle dolor crackalackin yo, we gonna chung adipiscing elit. Rizzle sapien velizzle, dope volutpizzle, suscipizzle bling bling, tellivizzle vizzle, funky fresh. Pellentesque eget tortizzle. Sizzle erizzle."
+  },
+]
+
+console.log(thumbnails);
