6e1e1760c9be5b5fb6bc6b461c22f7d50882d75b
David Pal <paldavid2030@gmail.com>
2018-12-11 14:48:43 +0100

    feat(giphy-api): add solution

---
+++ week-08/day-2/giphy-api/main.js
@@ -0,0 +1,28 @@
+//giphy API: rDWTzRS0iP5df4al0FbeklgfwboUta55
+
+const myKey = 'http://api.giphy.com/v1/gifs/search?q=big+lebowski&api_key=rDWTzRS0iP5df4al0FbeklgfwboUta55&limit=16'
+const myAnchor = document.createElement('a');
+const scr = document.querySelector('script');
+const myDiv = document.querySelector('.container');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', myKey, true);
+
+xhr.onload = function () {
+  const myData = JSON.parse(xhr.responseText).data;
+  console.log(myData[0]);
+
+  for (let i = 0; i < myData.length; i++) {
+    const myFrame = document.createElement('img');
+
+    myFrame.setAttribute('src', myData[i].images['480w_still'].url);
+    myFrame.setAttribute('width', 300);
+    myFrame.setAttribute('height', 200);
+    myDiv.appendChild(myFrame);
+
+    myFrame.onclick = function() {
+      myFrame.setAttribute('src', myData[i].images['downsized_medium'].url);
+    }
+  }
+}
+xhr.send();
