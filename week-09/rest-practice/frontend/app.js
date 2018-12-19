e0479bcb39ee243879422755e4cb4e2c3b6acb55
David Pal <paldavid2030@gmail.com>
2018-12-19 11:55:48 +0100

    feat(rest-backend-js):change port to 8080

--- week-09/rest-practice/frontend/app.js
+++ week-09/rest-practice/frontend/app.js
@@ -2,7 +2,13 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+const path = require('path');
 
-app.get('/', (req, res) => res.send("Helloka"));
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(port, () => console.log(`Yo dawgs, app is now listening on port ${port}`));
