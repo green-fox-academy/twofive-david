1680f92b4ca78f8696c273a31a01f2f53f3bf31d
David Pal <paldavid2030@gmail.com>
2018-12-19 12:23:36 +0100

    feat(rest-backend-js): add doubling

--- week-09/rest-practice/frontend/app.js
+++ week-09/rest-practice/frontend/app.js
@@ -11,4 +11,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+
+  const { input } = req.query;
+
+  if (input) {
+    res.json(
+      {
+        received: input,
+        result: input * 2,
+      }
+    )
+  } else {
+    res.json(
+      {
+        "error": "Please provide an input!"
+      }
+    )
+  }
+})
+
 app.listen(port, () => console.log(`Yo dawgs, app is now listening on port ${port}`));
