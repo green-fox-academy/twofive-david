c901d794d547ba95bc789e5684ef2eb11527db1f
David Pal <paldavid2030@gmail.com>
2019-01-02 12:51:44 +0100

    feat(reddit): insert data from frontend to database

--- week-08/reddit-project/server.js
+++ week-08/reddit-project/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
-const mySql  = require('mysql');
+const mySql = require('mysql');
 
 const conn = mySql.createConnection({
   host: process.env.host,
@@ -32,10 +32,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/posts', (req, res) => {
-  const sqlQuery = "SELECT * FROM mock_data ORDER BY score DESC LIMIT 10";
+  const sqlQuery = "SELECT * FROM mock_data";
 
   conn.query(sqlQuery, (err, data) => {
-    if(err) {
+    if (err) {
       res.status(500).send(err);
     }
     res.status(200).send(data);
@@ -47,7 +47,20 @@ app.get('/add-posts', (req, res) => {
 })
 
 app.post('/add-posts', (req, res) => {
-  console.log(req.body);
+  const newItemTitle = req.body.title;
+  const newItemUrl = req.body.url;
+  const date = 10;
+  const insertItem = 'INSERT INTO mock_data SET ?';
+  const item = { id: null, title: newItemTitle, url: newItemUrl, timestamp: date, score: 0 }
+
+  conn.query(insertItem, item, (err, response) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    console.log(response);
+  })
+
   res.send('megkaptam, koszi');
 })
 
