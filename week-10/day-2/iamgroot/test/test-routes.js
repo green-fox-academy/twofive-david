4eed06f3828840a9470ab8a7413ba13d2da5bf4c
David Pal <paldavid2030@gmail.com>
2019-01-08 13:45:23 +0100

    feat(groot): add testcase with no query

--- week-10/day-2/iamgroot/test/test-routes.js
+++ week-10/day-2/iamgroot/test/test-routes.js
@@ -4,7 +4,7 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
 
-test('Testing With Endpoint', (t) => {
+test('Testing With Query', (t) => {
   request(app)
   .get('/groot?message=hello')
   .expect(200)
@@ -15,6 +15,17 @@ test('Testing With Endpoint', (t) => {
   .end((err, res) => {
     if (err) throw err;
     t.same(res.body, { received: 'hello', translated: 'I am groot' }, 'ok');
+    t.end();
+  })
+})
+
+test('Testing Without Query', (t) => {
+  request(app)
+  .get('/groot')
+  .expect(202)
+  .expect({"error": "I am Groot!"})
+  .end((err, res) => {
+    t.same(res.body, {"error": "I am Groot!"}, 'not ok');
     t.end();
   })
 })
