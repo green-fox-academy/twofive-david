7a78ea425feae2fbe9b2a545669f6940f7e4c857
David Pal <paldavid2030@gmail.com>
2019-01-09 16:51:24 +0100

    feat(trialexam): connect database

--- week-10/day-3/orientaion-test-exam/assets/front-end.js
+++ week-10/day-3/orientaion-test-exam/assets/front-end.js
@@ -1,2 +1,16 @@
 'use strict'
 
+
+const xhr = new XMLHttpRequest();
+
+const sendData = () => {
+  xhr.open('POST', '/api/links');
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.onload = () => {
+    const response = (xhr.responseText);
+    console.log(response);
+  }
+  xhr.send(JSON.stringify({ url: 'hello' }));
+}
+
+sendData();
--- week-10/day-3/orientaion-test-exam/server.js
+++ week-10/day-3/orientaion-test-exam/server.js
@@ -1,8 +1,24 @@
 'use strict'
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const port = 8000;
+const mySql = require('mysql');
+const connection = mySql.createConnection({
+  host: process.env.HOST,
+  user: process.env.user,
+  database: process.env.DB,
+  password: process.env.PASS
+})
+
+connection.connect(err => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Connected to database', '\n');
+});
 
 app.use(express.json());
 app.use('/assets', express.static('assets'));
@@ -11,9 +27,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/api/links', )
+app.post('/api/links', (req,res) => {
+  const {url, alias} = req.body;
+
+})
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`)
 })
 
+// CREATE TABLE data (
+//   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//   url varchar(255),
+//   alias varchar(255),
+//   hitCount int,
+//   secretCode int
+// );
+
+// INSERT INTO data (url, alias, hitCount, secretCode) VALUES ('www.hello.com', 'hello', 10, 1111);
