5a381bcae4e4bde3f9499702af4d6bf47605e900
David Pal <paldavid2030@gmail.com>
2019-01-10 13:42:43 +0100

    feat(trialexam): add success message when item is posted to DB

--- week-10/day-3/orientaion-test-exam/assets/front-end.js
+++ week-10/day-3/orientaion-test-exam/assets/front-end.js
@@ -2,9 +2,10 @@
 
 const xhr = new XMLHttpRequest();
 const myForm = document.querySelector('form');
+const button = document.querySelector('.send');
 
 const sendData = () => {
-
+  const messageParagraph = document.querySelector('p.message');
   const { url, alias } = myForm.elements;
   const message = {
     url: url.value,
@@ -15,17 +16,28 @@ const sendData = () => {
   xhr.setRequestHeader('Content-type', 'application/json');
 
   xhr.onload = () => {
-    
+
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
-      console.log(response);
+      const { alias, secretcode } = response;
+      messageParagraph.textContent = `Your URL is aliased to ${alias} and your secret code is ${secretcode}`;
+      //console.log(alias, secretcode);
     } else {
       console.log('para');
     }
-
+    url.value = "";
+    alias.value = "";
   }
 
   xhr.send(JSON.stringify(message));
 }
 
-myForm.onsubmit = sendData;
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  sendData();
+})
+
+// onsubmit = (e) => {
+//   e.preventDefault();
+//   sendData(); 
+// }
